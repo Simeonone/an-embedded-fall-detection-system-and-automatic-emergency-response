ce442abc350ba2c341aabc187202fb18d2113886
Simeonone <simeon.kengere@gmail.com>
2024-07-10 21:37:24 +0300

    included the ability to send fall data to the server using HTTP POST request

--- falldetection/serial_communication.py
+++ falldetection/serial_communication.py
@@ -1,17 +1,45 @@
 import asyncio
 import json
+import requests
 from bleak import BleakClient, BleakScanner
 
 FALL_DATA_CHARACTERISTIC_UUID = "19b10001-e8f2-537e-4f6c-d104768a1214"
 device_address = "9e:52:0a:80:60:c6"  # Your Arduino's address
+server_url = 'http://localhost:8000/api/fall-detection-data/'
 
 def notification_handler(sender, data):
     try:
         decoded_data = json.loads(data.decode())
         print(f"Received data: {decoded_data}")
-        # Here you can add logic to handle fall detection
+        
+        # Check the predicted activity
         if decoded_data['predicted_activity'] == 'Fall':
-            print("FALL DETECTED!")
+            print("FALL DETECTED! Initiating emergency response.")
+            
+            # Prepare the fall detection data
+            fall_data = {
+                'sample_num': decoded_data['sample_num'],
+                'predicted_activity': decoded_data['predicted_activity'],
+                'accel_x': decoded_data['accel_x'],
+                'accel_y': decoded_data['accel_y'],
+                'accel_z': decoded_data['accel_z']
+            }
+            
+            try:
+                # Send the data to the server using HTTP POST request
+                response = requests.post(server_url, json=fall_data)
+                
+                # Check the response status code
+                if response.status_code == 201:
+                    print("Data sent to the server successfully.")
+                else:
+                    print("Failed to send data to the server.")
+                    print("Response status code:", response.status_code)
+                    print("Response content:", response.content)
+            except requests.exceptions.RequestException as e:
+                print("Error occurred while sending data to the server:", e)
+        else:
+            print(f"Current activity: {decoded_data['predicted_activity']}")
     except json.JSONDecodeError:
         print(f"Received non-JSON data: {data}")
 
